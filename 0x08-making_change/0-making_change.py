b45d86106377eb3bb897d4065bfcfdd31d7603b9
Aumizy <aumizy01@gmail.com>
2024-11-24 23:03:54 -0800

    0x08

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''solves the making change problem'''
+
+
+def makeChange(coins, total):
+    if total < 1:
+        return 0
+    coins.sort()
+    coins.reverse()
+    fewest = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        buff = total // coin
+        fewest += buff
+        total -= (buff * coin)
+    if total != 0:
+        return -1
+    return fewest
